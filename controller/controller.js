01573718599f9b195c0cd8c828667eadd5af6201
David Pham <david.pham@medela.ca>
2018-11-05 13:03:13 -0500

    updated config, controller

--- controller/controller.js
+++ controller/controller.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
 const db = require("../models/models");
 
+const cloudinary = require("cloudinary");
+const config = require("./config.json");
+
+cloudinary.config({
+  cloud_name: config.cloudinary.cloud_name,
+  api_key: config.cloudinary.api_key,
+  api_secret: config.cloudinary.api_secret
+});
+
 mongoose.connect(
   "mongodb://localhost:27017/r8it",
   { useNewUrlParser: true }
@@ -34,6 +43,17 @@ const queries = {
       .exec((err, posts) => {
         console.log(posts);
       });
+  },
+
+  getCloudinaryUrl: function(cloudinaryRef) {
+    return cloudinary.url(
+      cloudinaryRef,
+      config.cloudinary.standardTransformation
+    );
+  },
+
+  getComparables: function() {
+    db.Post.find().limit(2);
   }
 };
 
