975205f1120a3baa0059e0ea48dbf53b2123b9bf
David Pham <david.pham@medela.ca>
2018-11-05 11:56:14 -0500

    updated controller, added post react component, updated routes

--- controller/controller.js
+++ controller/controller.js
@@ -7,46 +7,6 @@ mongoose.connect(
 );
 
 const queries = {
-  seedChallenge: function() {
-    new db.Challenge({ verb: "loudest", noun: "jacket" })
-      .save()
-      .then(() => mongoose.disconnect());
-  },
-
-  seedUser: function() {
-    new db.User({
-      firstName: "David",
-      lastName: "Pham",
-      email: "email@davidpham.ca"
-    })
-      .save()
-      .then(() => mongoose.disconnect());
-  },
-
-  seedPost: function() {
-    new db.Post({
-      title: "Meet Rags!",
-      desc: "My son Jadyen and Rags in the snow.",
-      eloRank: 1200,
-      cloudinaryRef:
-        "little-kids-big-dogs-friendship-photography-andy-seliverstoff-2",
-      challengeId: "5bde84da7b201f1c6c8a3983",
-      userId: "5bde86950f837d2d88846725"
-    })
-      .save()
-      .then(() => mongoose.disconnect());
-  },
-
-  seedRating: function() {
-    new db.Rating({
-      pickedPostId: "5bdef8ad6a8ec03e5cd1a1e3",
-      unpickedPostId: "5bdef8d08c3a870f1445d248",
-      userId: "5bde86950f837d2d88846725"
-    })
-      .save()
-      .then(() => mongoose.disconnect());
-  },
-
   queryPost: function() {
     db.Post.find({})
       .populate("userId")
@@ -65,9 +25,16 @@ const queries = {
       .gte("dateCreated", new Date(2017, 01, 01))
       .populate("title")
       .exec((err, docs) => console.log(docs));
+  },
+
+  getPosts: function() {
+    db.Post.find()
+      .limit(2)
+      .sort("-eloRank")
+      .exec((err, posts) => {
+        console.log(posts);
+      });
   }
 };
 
-queries.queryPost();
-queries.queryRating();
-queries.queryRating2();
+queries.getPosts();
--- routes/someRoutes.js
+++ routes/someRoutes.js
@@ -2,8 +2,14 @@ const router = require("express").Router();
 
 router
   .all("/api")
-  .get((req, res) => {
-    res.send("home page");
+  .getComparables((req, res) => {
+    res.send({
+      subID: "submissionID",
+      imageName: "imageURL",
+      title: "imagetitle",
+      description: "imageDescription",
+      author: "imageAuthor"
+    });
   })
   .post((req, res) => {
     res.send("post route");
