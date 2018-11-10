88c641df26babba3d082bcd782cc326652a5287b
David Pham <email@davidpham.ca>
2018-11-10 09:58:13 -0500

    Navigation setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,20 @@ import Catgories from "./components/categories";
 import Posts from "./components/posts";
 import Challenge from "./components/challenge";
 import Submit from "./components/submit";
+import Navigation from "./components/navigation";
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<Catgories />
-				<Posts />
-				<Challenge />
-				<Submit />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Catgories />
+        <Posts />
+        <Challenge />
+        <Submit />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/navigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img
+          src="/docs/4.1/assets/brand/bootstrap-solid.svg"
+          width="30"
+          height="30"
+          class="d-inline-block align-top"
+          alt=""
+        />
+        Bootstrap
+      </a>
+    </nav>
+  );
+};
+
+export default Navigation;
