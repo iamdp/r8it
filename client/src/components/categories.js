a27ac9602e20d18284e6867ce0b989c5be02d826
tleytek <lucidmat94@gmail.com>
2018-11-12 22:28:01 -0500

    submit post feature, image uploading incomplete, user relation incomplete

--- client/src/components/categories.js
+++ client/src/components/categories.js
@@ -2,37 +2,37 @@ import React from "react";
 import API from "../utils/API";
 
 class Categories extends React.Component {
-  state = {};
+	state = {};
 
-  componentDidMount() {
-    API.getCategories().then(response => {
-      this.setState({ categories: response.data });
-      console.log(response.data);
-    });
-  }
+	componentDidMount() {
+		API.getCategories().then(response => {
+			this.setState({ categories: response.data });
+			console.log(response.data);
+		});
+	}
 
-  handleClick = () => {
-    API.getCategories().then(response => {
-      this.setState({ categories: response.data });
-    });
-  };
+	handleClick = () => {
+		API.getCategories().then(response => {
+			this.setState({ categories: response.data });
+		});
+	};
 
-  render() {
-    if (this.state.categories) {
-      return (
-        <div>
-          {this.state.categories.map(category => {
-            return (
-              <div>
-                <button>
-                  {category.verb} {category.noun}
-                </button>
-              </div>
-            );
-          })}
-        </div>
-      );
-    } else return null;
-  }
+	render() {
+		if (this.state.categories) {
+			return (
+				<div>
+					{this.state.categories.map(category => {
+						return (
+							<div key={category._id}>
+								<button>
+									{category.verb} {category.noun}
+								</button>
+							</div>
+						);
+					})}
+				</div>
+			);
+		} else return null;
+	}
 }
 export default Categories;
--- client/src/components/submit.js
+++ client/src/components/submit.js
@@ -1,34 +1,80 @@
 import React from "react";
+import API from "../utils/API";
 
 class Submit extends React.Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          <li>
-            Title
-            <input />
-          </li>
-          <li>
-            Description
-            <input />
-          </li>
-          <li>
-            cloudinaryRef
-            <input />
-          </li>
-          <li>
-            Challenge
-            <input />
-          </li>
-          <li>
-            UserId
-            <input />
-          </li>
-        </ul>
-      </div>
-    );
-  }
+	state = {
+		challenges: [],
+		title: "",
+		description: "",
+		cloudinaryRef: "placeholder",
+		challengeId: "",
+		userId: "placeholder"
+	};
+
+	componentDidMount = () => {
+		API.getCategories().then(response => {
+			this.setState({
+				challenges: response.data,
+				challengeId: response.data[0]._id
+			});
+		});
+	};
+
+	handleInputChange = event => {
+		const { name, value } = event.target;
+
+		this.setState({
+			[name]: value
+		});
+	};
+
+	handleSubmit = event => {
+		event.preventDefault();
+		API.submitPost(this.state).then(res => {
+			console.log(res);
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<form>
+					<label>
+						Title:
+						<input
+							type="text"
+							value={this.state.title}
+							name="title"
+							onChange={this.handleInputChange}
+						/>
+					</label>
+					<label>
+						Description:
+						<input
+							type="text"
+							value={this.state.description}
+							name="description"
+							onChange={this.handleInputChange}
+						/>
+					</label>
+					{/* Loop through the challenges and create an 'options' for each */}
+					<select onChange={this.handleInputChange}>
+						{this.state.challenges.map(challenge => {
+							return (
+								<option
+									key={challenge._id}
+									value={challenge._id}
+									name="challengeId"
+								>
+									{challenge.verb} {challenge.noun}
+								</option>
+							);
+						})}
+					</select>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default Submit;
